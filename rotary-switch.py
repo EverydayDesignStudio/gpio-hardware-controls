7183f76fc2cc193b037ecdd0e3e08669789e00bb
jordanrw <jordan_white@sfu.ca>
2019-04-19 23:04:44 -0700

    Add line to prevent potential bud while printing

--- rotary-switch.py
+++ rotary-switch.py
@@ -22,6 +22,7 @@ while True:
     output = '\n13: {}, \n19: {}, \n26: {}'.format(input13, input19, input26)
     time.sleep(0.05)
 
+    output = output.replace("\n", "\n\033[K")
     write(output)
     lines = len(output.split("\n"))
     write("\033[{}A".format(lines - 1))
