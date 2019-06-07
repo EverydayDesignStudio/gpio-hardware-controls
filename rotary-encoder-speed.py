da2caa2d4e8ed8f92cd89427720b97eb1dec580a
jordanrw <jordan_white@sfu.ca>
2019-06-07 16:09:11 -0700

    Add basic speed calculation for rotary encoder

---
+++ rotary-encoder-speed.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+from RPi import GPIO
+from datetime import datetime
+from time import sleep
+import queue
+
+PERIOD = 500
+
+clk = 21
+cnt = 20
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(cnt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+counter = 0
+clkLastState = GPIO.input(clk)
+
+last_time = datetime.now().timestamp()
+# q = queue.Queue(10)
+l = list()
+
+
+def calculate_speed():
+    global last_time
+    difference = round(datetime.now().timestamp() - last_time, 5)
+    # data sanitation: clean up random stray values that are extremely low
+    if difference < .001:
+        difference = .1
+
+    if len(l) > 10:
+        l.pop()
+    l.insert(0, difference)
+    average = sum(l)/len(l)
+
+    last_time = datetime.now().timestamp()
+
+    # print(difference)
+    # print('The average time between clicks is: {t}'.format(t=average))
+
+    #   .3   .07   .02
+    if average >= .3:
+        print('slow')
+    elif average >= .07 and average < .3:
+        print('medium')
+    elif average >= .02 and average < .07:
+        print('fast')
+    else:
+        print('super-duper fast')
+
+while True:
+    clkState = GPIO.input(clk)
+    cntState = GPIO.input(cnt)
+    if clkState != clkLastState:
+        if cntState != clkState:
+            counter += 1
+            calculate_speed()
+        else:
+            counter -= 1
+            calculate_speed()
+        print('this is the current counter: {c}\n'.format(c=counter))
+    clkLastState = clkState
