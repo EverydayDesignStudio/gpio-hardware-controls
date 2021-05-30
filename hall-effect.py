a6b10c8bfb8ec75549f0f5c89a6c5b7a7d765deb
Jordan White <jordanroywhite@gmail.com>
2021-05-30 15:02:55 -0700

    Add hall effect test based on Capra Projector tests

--- hall-effect.py
+++ hall-effect.py
@@ -7,25 +7,39 @@ from input_correct_data import *
 
 HALL_PIN = input_int('Input pin of sensor: ')
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(HALL_PIN, GPIO.IN)
-# GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setwarnings(False)
+GPIO.setup(HALL_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def write(line):
     sys.stdout.write(line)
     sys.stdout.flush()
 
-write("--- Hall Effect Sensor ---")
 
-while True:
-    input_hall = GPIO.input(HALL_PIN)
-    if input_hall is False:
-        output = '\nMagnet near'
-    else:
-        output = '\nMagnet far'
-    time.sleep(0.05)
-
+def full_write(output):
     output = output.replace("\n", "\n\033[K")
     write(output)
     lines = len(output.split("\n"))
     write("\033[{}A".format(lines - 1))
+
+
+write("----- Test Hall Effect Sensor -----")
+
+
+def main():
+    while True:
+        hall_effect_stat = ""
+
+        if GPIO.input(g.HALL_EFFECT_PIN) == 0:
+            hall_effect_stat = 'Magnet Near'
+
+        output = """
+Hall Effect:{he}
+""".format(he=hall_effect_stat)
+        full_write(output)
+
+        time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    main()
